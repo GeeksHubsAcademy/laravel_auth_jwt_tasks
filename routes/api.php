5f557a5664212beb9d68a45467a70fee9a20c44b
Dani <d.tarazoma@geekshubs.com>
2022-07-21 11:17:56 +0200

    feat: logout done

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/me', [AuthController::class, 'me'])->middleware('jwt.auth');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('jwt.auth');
