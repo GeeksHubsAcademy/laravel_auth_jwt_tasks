2da674cf8ff0a5c568001cc17efee4c0e463226f
Dani <d.tarazoma@geekshubs.com>
2022-07-22 12:56:49 +0200

    feat: is super admin middleware

--- routes/api.php
+++ routes/api.php
@@ -33,7 +33,7 @@ Route::group(["middleware" => "jwt.auth"] , function() {
     Route::get('/user/task/{id}', [TaskController::class, 'getUserByIdTask']);
 });
 
-Route::group(["middleware" => "jwt.auth"] , function() {
+Route::group(["middleware" => ["jwt.auth", "isSuperAdmin"]] , function() {
     Route::post('/user/super_admin/{id}', [UserController::class, 'addSuperAdminRoleToUser']);
     Route::post('/user/remove_super_admin/{id}', [UserController::class, 'removeSuperAdminRoleToUser']);
 });
